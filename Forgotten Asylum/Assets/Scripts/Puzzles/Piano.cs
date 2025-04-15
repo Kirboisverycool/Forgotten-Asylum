3043eb127b5c45a76cb579cb7975dc4d7bdbdd47
Kirboisverycool <1522838@edu.burnabyschools.ca>
2025-04-15 11:45:36 -0700

    4/15/25

--- Forgotten Asylum/Assets/Scripts/Puzzles/Piano.cs
+++ Forgotten Asylum/Assets/Scripts/Puzzles/Piano.cs
@@ -6,60 +6,70 @@ using TMPro;
 public class Piano : MonoBehaviour
 {
     [Header("Code will be around 7 digits")]
-    [SerializeField][Range(0, 9999999)] int correctCode;
-    [SerializeField] string codeText;
-    [SerializeField] TextMeshProUGUI codeTextTMP;
+    [SerializeField] List<int> correctCode = new List<int>();
+    [SerializeField] int codeLength = 7;
+    [SerializeField] int currentNumber = -1;
+    [SerializeField] List<int> codeText = new List<int>();
+    [SerializeField] int finalDigit;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool finishedPuzzle = false;
+
+    public void PressButton(int num)
     {
-        codeText = "";
-        UpdateDisplay();
-    }
-    public void pressButton(int num)
-    {
-        if (codeText.Length < 4)
+        if(!finishedPuzzle)
         {
-            codeText += num.ToString();
-            UpdateDisplay();
-            if (codeText.Length == 4)
+            if (codeText.Count < codeLength)
             {
-                CheckIfCorrect(codeText);
+                codeText.Add(num);
+                currentNumber++;
+
+                CheckIfCorrect();
             }
         }
     }
-    private void UpdateDisplay()
-    {
-        codeTextTMP.text = codeText;
-    }
-    private void CheckIfCorrect(string codeTested)
-    {
-        if (codeTested == correctCode.ToString())
-        {
-            Debug.Log("CorrectCode");
-            StartCoroutine(CorrectCode());
-        }
-        else
-        {
-            Debug.Log("Incorect");
-            StartCoroutine(WrongCode());
 
+    public void PlaySound(AudioClip audioClip)
+    {
+        //play sound
+    }
+
+    private void CheckIfCorrect()
+    {
+        if(!finishedPuzzle)
+        {
+            if (codeText[currentNumber] == correctCode[currentNumber])
+            {
+                Debug.Log("correct");
+
+                if (codeText.Count == finalDigit)
+                {
+                    Debug.Log("You Got It");
+                    StartCoroutine(CorrectCode());
+                }
+            }
+            else
+            {
+                StartCoroutine(WrongCode());
+            }
         }
     }
     IEnumerator WrongCode()
     {
         yield return new WaitForSeconds(1);
-        codeText = "";
-        UpdateDisplay();
+        codeText.Clear();
+        currentNumber = -1;
+        Debug.Log("incorrect");
     }
     IEnumerator CorrectCode()
     {
         yield return new WaitForSeconds(1);
+        finishedPuzzle = true;
+        codeText.Clear();
         CloseTab();
         //DoActionFromCode
     }
     public void CloseTab()
     {
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
