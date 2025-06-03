8b89bdd6facd6a6d4c729600b5760fcc4ddb27ca
Kirboisverycool <1522838@edu.burnabyschools.ca>
2025-06-03 11:44:33 -0700

    6/3/25

--- Forgotten Asylum/Assets/Scripts/Heartbeat.cs
+++ Forgotten Asylum/Assets/Scripts/Heartbeat.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class Heartbeat : MonoBehaviour
 {
-    EnemyAI enemy;
-    [SerializeField] Vector2 distanceFromEnemy;
+    float distance;
+    float clampedDistance;
+
+    [SerializeField] float heartbeatVolume;
+    [SerializeField] float heartbeatPitch;
+    [SerializeField] float maxDistance = 15;
+    [SerializeField] float minDistance = 0;
+    [SerializeField] float maxPitch;
+    [SerializeField] float minPitch;
+
+    AudioSource heartbeatAudio;
+    GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemy = FindObjectOfType<EnemyAI>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        heartbeatAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        distance = Vector3.Distance(player.transform.position, transform.position);
 
+        clampedDistance = Mathf.Clamp01((distance - maxDistance) / (minDistance - maxDistance));
 
-        //Debug.Log(distanceFromEnemy);
+        heartbeatVolume = Mathf.Lerp(0f, 1f, clampedDistance);
+        heartbeatPitch = Mathf.Lerp(minPitch, maxPitch, clampedDistance);
+
+        heartbeatAudio.volume = heartbeatVolume;
+        heartbeatAudio.pitch = heartbeatPitch;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, maxDistance);
     }
 }
