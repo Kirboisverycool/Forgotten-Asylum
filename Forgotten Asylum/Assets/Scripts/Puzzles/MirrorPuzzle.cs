d3644e851ce34072a9b18addd16e68b313c6d6e3
KyojuroB <grizzlypandastudio@gmail.com>
2025-06-15 02:06:14 -0700

    6/15

--- Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
+++ Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class MirrorPuzzle : MonoBehaviour
 {
-
+    List<RotateMirrors> mirrorsList;
+    public void CheckList()
+    {
+        for (int i = 0; i < mirrorsList.Count; i++)
+        {
+            if (mirrorsList[i].isCorrect)
+            mirrorsList[i + 1].lightObj.SetActive(true);
+        }
+    }
 
 }
--- Forgotten Asylum/Assets/Scripts/Puzzles/RotateMirrors.cs
+++ Forgotten Asylum/Assets/Scripts/Puzzles/RotateMirrors.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class RotateMirrors : MonoBehaviour
 {
-    [SerializeField] Sprite mirrorSprites;
-    
+    [SerializeField] List<Sprite> mirrorSprites;
+    [SerializeField] List<int> lightsRotation;
+ 
+    [SerializeField] int currentIndex = 1;
+    [SerializeField] SpriteRenderer mirrorRenderer;
+    [SerializeField]public GameObject lightObj;
+    [SerializeField] int correctePosition;
+    [SerializeField]public bool isCorrect;
+    private void Start()
+    {
+        mirrorRenderer.sprite = mirrorSprites[currentIndex];
 
-  
+        lightObj.transform.eulerAngles = new Vector3(0,0, lightsRotation[currentIndex]);
+        gameObject.SetActive(false);
+    }
+    private void OnEnable()
+    {
+       
+        Debug.Log("MirrorRotate");
+        if (currentIndex + 2 > mirrorSprites.Count)
+        {
+            currentIndex = 0;
+        }
+        else
+        {
+            currentIndex++;
+        }
+        lightObj.transform.eulerAngles = new Vector3(0, 0, lightsRotation[currentIndex]);
+        mirrorRenderer.sprite = mirrorSprites[currentIndex];
+        if (currentIndex == correctePosition)
+        {
+            isCorrect = true;
+        }
+        else
+        { 
+            isCorrect = false;
+        }
+        gameObject.SetActive(false);
+    }
+
+
 }
