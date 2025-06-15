a5b7c2101f075047e3df137489ca0b1e67437dfd
KyojuroB <grizzlypandastudio@gmail.com>
2025-06-14 20:16:12 -0700

    6/14

--- Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
+++ Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
@@ -4,54 +4,6 @@ using UnityEngine;
 
 public class MirrorPuzzle : MonoBehaviour
 {
-    [SerializeField] List<RotateMirrors> mirrors;
 
-    public void CheckForCorrect()
-    {
-        for (int i = 0; i < mirrors.Count; i++)
-        {
-            if (mirrors[i].isInRotationRange)
-            {
-                if (i + 1 > mirrors.Count)
-                {
-                    Debug.Log("FinishedMirrorPuzzle");
-                }
-                else
-                {
-                    mirrors[i + 1].reflectionLight.SetActive(true);
-                }
-              
-            }
-            else
-            {
-                Debug.Log("Negative");
-                for (int j = i; j < mirrors.Count; j++)
-                {
-                    if (j + 1 > mirrors.Count)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        Debug.Log("SetLightFalse" + j + "," + i + mirrors[j + 1].gameObject);
-                        mirrors[j + 1].reflectionLight.SetActive(false);
-                        
-                    }
-                   
-                }
-                return;
-               
-            }
-
-        }
-    }   
-    private void Start()
-    {
-        
-    }
-    private void Update()
-    {
-        
-    }
 
 }
