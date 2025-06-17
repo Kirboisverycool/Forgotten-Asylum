14e6c7404a78e2fd276a834cedf6090d239695a4
KyojuroB <grizzlypandastudio@gmail.com>
2025-06-16 23:37:04 -0700

    6/16

--- Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
+++ Forgotten Asylum/Assets/Scripts/Puzzles/MirrorPuzzle.cs
@@ -6,35 +6,44 @@ using UnityEngine;
 public class MirrorPuzzle : MonoBehaviour
 {
     [SerializeField] List<RotateMirrors> mirrorsList;
-   
+    [SerializeField] GameObject secretEntrance;
     public void CheckList()
     {
-        for (int i = 0; i < mirrorsList.Count; i++)
+        if (mirrorsList.Count == 0) return;
+
+        mirrorsList[0].lightObj.SetActive(true);
+
+        bool allCorrect = mirrorsList[0].isCorrect; // Start with the first
+
+        for (int i = 1; i < mirrorsList.Count; i++)
         {
-            if (mirrorsList[i].isCorrect)
+            if (mirrorsList[i - 1].isCorrect)
             {
-                if (i >= mirrorsList.Count)
-                {
-                    Debug.Log("ALL CORRECT");
-                }
-                else
-                {
-                    mirrorsList[i + 1].lightObj.SetActive(true);
-                }
-               
+                mirrorsList[i].lightObj.SetActive(true);
+                allCorrect = allCorrect && mirrorsList[i].isCorrect;
             }
             else
             {
-                i++;
+                // Turn off this and everything after
                 for (int j = i; j < mirrorsList.Count; j++)
                 {
-                    mirrorsList[i].lightObj.SetActive(false);
+                    mirrorsList[j].lightObj.SetActive(false);
                 }
-                return;
-
+                allCorrect = false;
+                break;
             }
-          
         }
+
+        if (allCorrect)
+        {
+            OnPuzzleComplete();
+        }
+    }
+
+    private void OnPuzzleComplete()
+    {
+        Debug.Log("Puzzle Completed! All mirrors are correct.");
+        secretEntrance.SetActive(true); 
     }
 
 }
